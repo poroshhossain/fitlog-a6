7df86a69229768ed936d7dcd08c2894ac69d7e17
Md Porosh Hossain <poroshdevhub@gmail.com>
2026-09-24 17:16:53 +0600

    added to Footer section design complate

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,26 @@
+import FooterLogo from '@/assets/footer-logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
 const Footer = () => {
-  return (
-    <footer>Footer</footer>
-  )
+    const footerItem = {
+        name: 'footer logo',
+        path: '/',
+        src: FooterLogo,
+        copyright: '© 2026 FitLog — Workout Library. Train hard, log honest.'
+    }
+    return (
+        <footer className="bg-cDark py-6">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between items-center">
+                    <Link href={`${footerItem.path}`} className="flex items-center gap-2" >
+                        <Image src={footerItem.src} className='w-5' alt={footerItem.name} />
+                        <span className='text-cLight text-[14px] font-bold font-Oswald capitalize'>FITLOG</span>
+                    </Link>
+
+                    <p className='text-cLight/50 text-[12px] font-Inter capitalize'>{footerItem.copyright}</p>
+                </div>
+            </div>
+        </footer>
+    )
 }
 export default Footer
